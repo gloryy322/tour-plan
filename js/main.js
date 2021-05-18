5a5ee7a90fe63ca97173b1b1d5bfd7e476c98eb4
gloryy322 <stratienko77@gmail.com>
2021-05-18 09:38:48 +0300

    Cделал мобильное меню

--- js/main.js
+++ js/main.js
@@ -26,3 +26,11 @@ const reviewwSwiper = new Swiper('.rewievs-slider', {
     prevEl: '.rewievs-slider__button--prev',
   },
 });
+
+let menuButton = document.querySelector('.header__button-menu');
+let headerMenu = document.querySelector('.header__menu');
+
+menuButton.addEventListener('click', function () {
+  headerMenu.classList.toggle('header__menu--visible');
+  console.log('Клик по кнопке');
+});
